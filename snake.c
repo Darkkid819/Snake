e9af8f44b93650b9d09a8f67c8032b3c8078d7f7
Darkkid819 <darkkid8192004@gmail.com>
2024-04-21 23:00:57 -0400

    added game logic

--- snake.c
+++ snake.c
@@ -7,7 +7,7 @@
 #define SNAKE_SIZE 20
 #define FOOD_SIZE 20
 #define SNAKE_INITIAL_LENGTH 1
-#define MOVE_SPEED 20
+#define MOVE_INTERVAL 5
 #define SNAKE_MAX_LENGTH ((SCREEN_WIDTH * SCREEN_HEIGHT) / (SNAKE_SIZE * SNAKE_SIZE))
 
 typedef enum GameScreen {
@@ -33,6 +33,7 @@ static Texture2D texLogo;
 
 static Font font;
 
+static int moveCounter;
 static Snake snake;
 static Food food;
 
@@ -77,6 +78,8 @@ static void InitGame(void) {
     screen = LOGO;
     framesCounter = 0;
 
+    moveCounter = MOVE_INTERVAL;
+
     snake.bodySize = SNAKE_INITIAL_LENGTH;
     snake.maxLength = SNAKE_MAX_LENGTH;
     snake.body = (Vector2*) malloc(snake.maxLength * sizeof(Vector2));
@@ -111,8 +114,63 @@ static void UpdateGame(void) {
             }
         } break;
         case GAMEPLAY: {
-            if(IsKeyPressed(KEY_ENTER)) {
-                screen = ENDING;
+
+            Vector2 potentialDirection = snake.direction;
+            if (IsKeyDown(KEY_RIGHT) && snake.direction.x == 0) {
+                potentialDirection = (Vector2){SNAKE_SIZE, 0};
+            } else if (IsKeyDown(KEY_LEFT) && snake.direction.x == 0) {
+                potentialDirection = (Vector2){-SNAKE_SIZE, 0};
+            } else if (IsKeyDown(KEY_UP) && snake.direction.y == 0) {
+                potentialDirection = (Vector2){0, -SNAKE_SIZE};
+            } else if (IsKeyDown(KEY_DOWN) && snake.direction.y == 0) {
+                potentialDirection = (Vector2){0, SNAKE_SIZE};
+            }
+
+            if (--moveCounter > 0) {
+                return;
+            }
+            moveCounter = MOVE_INTERVAL;
+
+
+            Vector2 newHeadPosition = (Vector2){
+                snake.body[0].x + snake.direction.x,
+                snake.body[0].y + snake.direction.y
+            };
+
+            if (potentialDirection.x != -snake.direction.x || potentialDirection.y != -snake.direction.y) {
+                snake.direction = potentialDirection;
+            }
+
+            if (newHeadPosition.x < 0 
+                || newHeadPosition.y < 0 
+                || newHeadPosition.x >= SCREEN_WIDTH
+                || newHeadPosition.y >= SCREEN_HEIGHT) {
+                    screen = ENDING;        
+            }
+
+            for (int i = 1; i < snake.bodySize; i++) {
+                if (newHeadPosition.x == snake.body[i].x && newHeadPosition.y == snake.body[i].y) {
+                    screen = ENDING;
+                }
+            }
+
+            for (int i = snake.bodySize - 1; i > 0; i--) {
+                snake.body[i] = snake.body[i-1];
+            }
+            snake.body[0] = newHeadPosition;
+
+            if (newHeadPosition.x == food.position.x && newHeadPosition.y == food.position.y) {
+                snake.bodySize++;
+                snake.score++;
+                if (snake.bodySize < snake.maxLength) {
+                    snake.body = realloc(snake.body, snake.bodySize * sizeof(Vector2));
+                    snake.body[snake.bodySize - 1] = newHeadPosition;
+                }
+
+                food.position = (Vector2){
+                    SNAKE_SIZE * (GetRandomValue(0, (SCREEN_WIDTH - FOOD_SIZE) / SNAKE_SIZE)), 
+                    SNAKE_SIZE * (GetRandomValue(0, (SCREEN_HEIGHT - FOOD_SIZE) / SNAKE_SIZE))
+                };
             }
         } break;
         case ENDING: {
@@ -156,7 +214,7 @@ static void DrawGame(void) {
             DrawRectangleV(food.position, (Vector2){FOOD_SIZE, FOOD_SIZE}, RED);
         } break;
         case ENDING: {
-            DrawTextEx(font, TextFormat("SCORE: %d", snake.score), (Vector2){SCREEN_WIDTH / 2 - MeasureText("END", 80) / 2, 100}, 80, 6, MAROON);
+            DrawTextEx(font, TextFormat("SCORE: %d", snake.score), (Vector2){SCREEN_WIDTH / 2 - MeasureText(TextFormat("SCORE: %d", snake.score), 80) / 2, 100}, 80, 6, MAROON);
                         
             if ((framesCounter) % 2 == 0) {
                 DrawText("PRESS [ENTER] TO PLAY AGAIN", 
